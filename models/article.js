fcf955a8f8bb57197390f4247da3c6215de485bb
bttf <adnanchowdhury88@gmail.com>
2014-09-17 17:53:07 -0400

    api now functioning with elasticsearch

---
+++ models/article.js
@@ -0,0 +1,71 @@
+var esClient = require('../lib/esClient');
+var article = {};
+
+var index = 'ac',
+    type = 'article';
+
+article.fetch = function(id) {
+  return esClient.get({
+    index: index,
+    type: type,
+    id: id
+  }).then(function(body) {
+    var article = body._source;
+    article.id = body._id;
+    return article;
+  });
+};
+
+article.fetchAll = function() {
+  return esClient.search({
+    index: index,
+    type: type,
+    q: '*'
+  }).then(function(body) { 
+    var resp = {
+      'articles': []
+    };
+    for (var i = 0; i < body.hits.hits.length; i++) {
+      var article = body.hits.hits[i]._source;
+      article.id = body.hits.hits[i]._id;
+      resp.articles.push(article);
+    }
+    return resp;
+  });
+};
+
+article.create = function(data) {
+  return esClient.create({
+    index: index,
+    type: type,
+    body: {
+      title: data.title || "",
+      tags: data.tags || "",
+      body: data.body || "",
+      url: data.url || "",
+      type: data.type || "",
+      createdAt: data.createdAt || (new Date()).toISOString()
+    }
+  });
+};
+
+article.update = function(id, data) {
+  return esClient.update({
+    index: index,
+    type: type,
+    id: id,
+    body: {
+      doc: data
+    }
+  });
+};
+
+article.delete = function(id) {
+  return esClient.delete({
+    index: index,
+    type: type,
+    id: id
+  });
+};
+
+module.exports = article;
---
+++ routes/api.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+var article = require('../models/article');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.json({
+    message: 'hey, you made it. great job shithead'
+  });
+});
+
+router.get('/articles', function(req, res) {
+  article.fetchAll().then(
+    function (data) {
+      res.json(data);
+    }, 
+    function (err) {
+      console.log('error ' + err.message);
+      res.status(500).send(err);
+    });
+});
+
+router.get('/articles/:id', function(req, res) {
+  article.fetch(req.params.id).then(
+    function (data) {
+      res.json(data);
+    }, 
+    function (err) {
+      console.log('error ' + err.message);
+      res.status(500).send(err);
+    });
+});
+
+router.post('/articles', function(req, res) {
+  article.create(req.body).then(
+    function() { res.status(200).send('OK'); },
+    function(err) { res.status(500).send(err); }
+  );
+});
+
+router.put('/articles/:id', function(req, res) {
+  article.update(req.params.id, req.body).then(
+    function() { res.status(200).send('OK'); },
+    function(err) { res.status(500).send(err); }
+  );
+});
+
+router.delete('/articles/:id', function(req, res) {
+  article.delete(req.params.id).then(
+    function() { res.status(200).send('OK'); },
+    function(err) { res.status(500).send(err); }
+  );
+});
+
+module.exports = router;
+
